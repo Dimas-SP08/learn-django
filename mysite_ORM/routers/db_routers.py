944b4c7f175cb3e24b49598fb729cc899f9b9032
Dimas SP <satriapasha269@gmail.com>
2025-10-24 18:21:59 +0700

    learn-django

---
+++ mysite_ORM/routers/db_routers.py
@@ -0,0 +1,44 @@
+class Kontak():
+    route_app_labels = {'Kontak'}
+
+    def db_for_read(self,model,**hitns):
+        if model._meta.app_label in self.route_app_labels:
+            return "db_Kotak"
+        return None
+    
+    def db_for_write(self,model,**hitns):
+        if model._meta.app_label in self.route_app_labels:
+            return "db_Kotak"
+        return None
+    
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        if app_label in self.route_app_labels:
+            return db == "db_Kotak"
+        return None
+    
+
+    
+class Blog():
+    route_app_labels = {"blog"}
+
+    def db_for_read(self,model,**hitns):
+        if model._meta.app_label in self.route_app_labels:
+            return "db_blog"
+        return None
+    
+    def db_for_write(self,model,**hitns):
+        if model._meta.app_label in self.route_app_labels:
+            return "db_blog"
+        return None
+    
+    def allow_relation(self,obj1,obj2,**hints):
+
+        if (obj1._meta.app_label in self.route_app_labels
+            or obj2._meta.app_label in self.route_app_labels):
+            return True
+        return None
+    
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        if app_label in self.route_app_labels:
+            return db == "db_blog"
+        return None
